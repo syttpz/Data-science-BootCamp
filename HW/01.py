832ac9afee011d46065d73530a6574895a01df75
Eddy217 <bhu7035@gmail.com>
2025-10-05 15:40:21 +0400

    first commit

---
+++ HW/01.py
@@ -0,0 +1,12 @@
+'''
+Homework Questions
+1. Write a program that takes a word as an input and print the number of vowels in the word.
+2. Iterate through the following list of animals and print each one in all caps.
+
+  animals=['tiger', 'elephant', 'monkey', 'zebra', 'panther']
+
+3. Write a program that iterates from 1 to 20, printing each number and whether it's odd or even.
+4. Write a program to check if a string is a palindrome or not.
+5. Write a function sum_of_integers(a, b) that takes two integers as input from the user and returns their sum.
+'''
+
