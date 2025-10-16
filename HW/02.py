0682c082b25ded27a7cf026c5dd99f355525880b
Eddy217 <bhu7035@gmail.com>
2025-10-16 19:19:09 +0400

    hw2

---
+++ HW/02.py
@@ -0,0 +1,15 @@
+'''
+Homework Questions
+
+    Define two custom numpy arrays, say A and B. Generate two new numpy arrays by stacking A and B vertically and horizontally.
+    Find common elements between A and B. [Hint: Intersection of two sets]
+    Extract all numbers from A which are within a specific range. eg between 5 and 10. [Hint: np.where() might be useful or boolean masks]
+    Filter the rows of iris_2d that has petallength (3rd column) > 1.5 and sepallength (1st column) < 5.0
+
+'''
+import numpy as np
+
+
+
+if __name__ == "__main__":
+    print("Hello World")
